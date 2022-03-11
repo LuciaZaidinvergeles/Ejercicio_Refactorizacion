02c45e81057384083d869db14c56077665324406
Lucía Domínguez Vera <ldominguez39@ieszaidivergeles.org>
2022-03-11 09:54:32 +0100

    Elimina de la clase NoCambiaLongitud los métodos Modificación, inserción y borrado. Como véis no deberíamos borrar Modificación pero lo hacemos simulando un error.

--- Prueba_/src/prueba_/NoCambiaLongitud.java
+++ Prueba_/src/prueba_/NoCambiaLongitud.java
@@ -1,4 +1,4 @@
-    package prueba_;
+package prueba_;
 
 public class NoCambiaLongitud {
 
@@ -19,49 +19,7 @@ public class NoCambiaLongitud {
     private int valor;
 //metodo que modifica el contenido de la posicion p de un array con el valor pasado
 
-    public void modificar(int[] vector, int LongitudActual, int posicion) {
-        int i;
-        final int longitud = vector.length;
-//Modificamos el array
-        if (posicion < longitud) {
-            System.out.println("Elemento a modificar=" + vector[posicion]);
-            vector[posicion] = this.getValor();
-        }
-//Mostramos los elementos del array
-         Visualizacion(longitud, vector);
-    }
-//metodo que borra el elemento de la posicion p desplazando a la izquierda todos los elementos
-
-    public void borrar(int[] vector, int LongitudActual, int posicion) {
-        int i;
-        final int longitud = vector.length;
-//Borramos el elemento
-        if (posicion < longitud) {
-            System.out.println("Elemento a borrar=" + vector[posicion]);
-            for (i = posicion; i < longitud - 1; i++) {
-                vector[i] = vector[i + 1];
-            }
-        }
-//Mostramos los elementos del array
-         Visualizacion(longitud, vector);
-    }
-//metodo que inserta un elemento en la posicion p desplazando a la derecha todos los elementos
-
-    public void insertar(int[] vector, int LongitudActual, int posicion) {
-        int i;
-        final int longitud = vector.length;
-//Borramos el elemento
-        if (posicion < longitud) {
-            System.out.println("Elemento a insertar=" + this.getValor());
-            for (i = longitud - 1; i > posicion; i--) {
-                vector[i] = vector[i - 1];
-            }
-            vector[posicion] = this.getValor();
-        }
-        Visualizacion(longitud, vector);
-    }
-
-    public void Visualizacion(int longitud, int[] vector) {
+       public void Visualizacion(int longitud, int[] vector) {
         //Mostramos los elementos del array
         System.out.print("v={");
         for (int i = 0; i < vector.length; i++) {
